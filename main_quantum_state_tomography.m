e6b89339b12567277e38dfa0e2a2fd8cc636e8f8
Haotong Duan <15102633721@163.com>
2026-03-19 17:57:53 +0800

    Update main_quantum_state_tomography.m

--- main_quantum_state_tomography.m
+++ main_quantum_state_tomography.m
@@ -5,7 +5,7 @@ samples = samples_train;
 n=size(samples,2);
 Dmax=400; 
 n_batches=1; 
-mps=MPS_train_riemann2_dec_end_test(n,samples',n_batches);
+mps=UMPS_SD(n,samples',n_batches);
 mps.max_bondim=Dmax;
 mps.learning_rate=0.001; 
 mps.train(8); 
